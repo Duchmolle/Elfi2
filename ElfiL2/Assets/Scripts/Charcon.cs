7c9011c2983bb3e9b84240d156ce4eaaef453d45
Victor <victordesenneville@gmail.com>
2023-02-22 19:26:29 +0100

    Linking FMOD with Unity project

--- ElfiL2/Assets/Scripts/Charcon.cs
+++ ElfiL2/Assets/Scripts/Charcon.cs
@@ -19,6 +19,7 @@ public class Charcon : MonoBehaviour
         tr = transform;
         rb = GetComponent<Rigidbody>();
         groundPosition = tr.position;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
